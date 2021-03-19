8a214271fa936420232a1c1eba47239d3c4ddca6
DAMILARE1012 <56843419+DAMILARE1012@users.noreply.github.com>
2021-03-19 12:08:04 +0100

    New Changes

--- resources/views/pages/reviews.blade.php
+++ resources/views/pages/reviews.blade.php
@@ -26,38 +26,44 @@
     <section id="testimonials" class="padd-section text-center" style="margin-bottom: 40px;">
         <div class="container" data-aos="fade-up">
             <div class="row justify-content-center">
-                @if($review->count() > 0)
-                        @foreach($review as $row)
-                <div class="col-md-4">
+                @if ($review->count() > 0)
+                    @foreach ($review as $row)
+                        <div class="col-md-4">
 
-                    <div class="testimonials-content">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <div class="top-top">
+                            <div class="testimonials-content">
+                                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                                    <div class="top-top">
 
-                                <fieldset class="supaview__rating">
-                                        <input type="radio" id="star5" name="rate" value="5" / active><label for="star5"></label>
-                                        <input type="radio" id="star4" name="rate" value="4" /><label for="star4"></label>
-                                        <input type="radio" id="star3" name="rate" value="3" /><label for="star3"></label>
-                                        <input type="radio" id="star2" name="rate" value="2" /><label for="star2"></label>
-                                        <input type="radio" id="star1" name="rate" value="1" /><label for="star1"></label>
-                                    </fieldset>
-                                <p>{{ $row->message }}</p>
-                                <h4>{{ $row->name }}<span>{{ \Carbon\Carbon::parse($row->created_at)->diffForHumans() }}</span></h4>
+                                        <fieldset class="supaview__rating">
+                                            <input type="radio" id="star5" name="rate" value="5" / active><label
+                                                for="star5"></label>
+                                            <input type="radio" id="star4" name="rate" value="4" /><label
+                                                for="star4"></label>
+                                            <input type="radio" id="star3" name="rate" value="3" /><label
+                                                for="star3"></label>
+                                            <input type="radio" id="star2" name="rate" value="2" /><label
+                                                for="star2"></label>
+                                            <input type="radio" id="star1" name="rate" value="1" /><label
+                                                for="star1"></label>
+                                        </fieldset>
+                                        <p>{{ $row->message }}</p>
+                                        <h4>{{ $row->name }}<span>{{ \Carbon\Carbon::parse($row->created_at)->diffForHumans() }}</span>
+                                        </h4>
 
                                     </div>
-                        
+
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                 @endforeach
-                        @else
-                        <h6 class="text-center text-danger">No Reviews!</h6>
-                    @endif
-                       
-                    </div>
-                </div>
+                    @endforeach
+                @else
+                    <h6 class="text-center text-danger">No Reviews!</h6>
+                @endif
 
             </div>
+        </div>
+
+        </div>
         </div>
     </section>
 
@@ -65,24 +71,19 @@
     <section id="repairs" class="news-single section">
         <div class="container">
             <div class="row">
-                
                 <div class="col-md-8" id="review">
                     <div class="col-12 supaviews">
                         <div class="supaviews__add">
                             <div class="supaview">
-<<<<<<< HEAD
-                                <h1 class="supaview__title" style="text-align: center; color: black; margin-bottom: 10px;">Add a new review</h1>
-                                <form id="review" method="POST" action="#">
-=======
-                                <h1 class="supaview__title" style="text-align: center; color: black; margin-bottom: 10px;">Add Your review</h1>
-                                @if(Session::has('success'))
-                                  <div class="alert  alert-success alert-dismissible fade show">
-                                      <span class="badge badge-pill badge-success">Success</span>
-                                      {{ Session::get('success') }}
-                                  </div>
+                                <h1 class="supaview__title" style="text-align: center; color: black; margin-bottom: 10px;">
+                                    Add Your review</h1>
+                                @if (Session::has('success'))
+                                    <div class="alert  alert-success alert-dismissible fade show">
+                                        <span class="badge badge-pill badge-success">Success</span>
+                                        {{ Session::get('success') }}
+                                    </div>
                                 @endif
                                 <form id="review" method="POST" action=" {{ route('store.review') }} ">
->>>>>>> 70b1787a654e4a9cff128ef56154f1086cf2c0de
                                     @csrf
                                     <fieldset class="supaview__rating">
                                         <input type="radio" id="star5" name="rate" value="5" /><label for="star5"></label>
@@ -92,24 +93,21 @@
                                         <input type="radio" id="star1" name="rate" value="1" /><label for="star1"></label>
                                     </fieldset>
                                     <div class="supaview__copy">
-                                        <input type="text" name="name" placeholder="Name"/ required>
-                                        <input type="email" name="email" placeholder="Email Address"/ required>
+                                        <input type="text" name="name" placeholder="Name" / required>
+                                        <input type="email" name="email" placeholder="Email Address" / required>
                                         <textarea name="message" placeholder="Message" rows="5" required></textarea>
                                     </div>
                                     <button class="supaview__submit">Submit review</button>
                                 </form>
                             </div>
                         </div>
-<<<<<<< HEAD
-=======
-                        
+
                         <div class="supaviews__list">
                             <div class="supaview" style="color: black;"></div>
                         </div>
-                        
->>>>>>> 70b1787a654e4a9cff128ef56154f1086cf2c0de
+
                     </div>
-                    
+
                 </div>
             </div>
         </div>
